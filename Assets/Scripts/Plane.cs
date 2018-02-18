edac2ca471fec5c22dcfa6d0ab70754047e41264
August Dominik Raaschou-Pedersen <guciodevs@gmail.com>
2018-02-18 17:42:33 +0100

    Video 3 - Rocket Movement & Restart on Collision

---
+++ Assets/Scripts/Plane.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour {
+
+    public float speed = 10;
+    public float turnSpeed = 10;
+
+    private Rigidbody2D rb2d;
+
+    private Camera cam;
+
+    private Vector2 mousePos;
+    private Vector2 dir;
+
+    void Start()
+    {
+        cam = Camera.main;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //Rotate the plane towards mouse
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        dir = (mousePos - (Vector2)transform.position).normalized;
+  
+        transform.up = Vector2.Lerp(transform.up, dir, turnSpeed * Time.fixedDeltaTime);
+
+        //Move plane forward
+        rb2d.velocity = transform.up * speed;
+
+    }
+
+    public void Kill()
+    {
+        print("Oh no! we died :(");
+
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+
+    public float speed = 10;
+    public float turnSpeed = 10;
+
+    private Rigidbody2D rb2d;
+
+    private Vector2 dir;
+    private Transform planeTransform;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        planeTransform = FindObjectOfType<Plane>().transform;
+    }
+
+    void FixedUpdate()
+    {
+        //Rotate towards plane
+        dir = ((Vector2)planeTransform.position - (Vector2)transform.position).normalized;
+
+        transform.up = Vector2.Lerp(transform.up, dir, turnSpeed * Time.fixedDeltaTime);
+
+        //Move plane forward
+        rb2d.velocity = transform.up * speed;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Plane")
+        {
+            col.GetComponent<Plane>().Kill();
+        }
+
+    }
+}
